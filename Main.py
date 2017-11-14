5797b2f6b20c751c4f7ff3c1f3ef3787746256fe
leo019527 <15281070@bjtu.edu.cn>
2017-11-14 11:00:27 +0800

    Initial commit

---
+++ Main.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+import sys
+import pygame
+from pygame.locals import *
+
+# INIT
+pygame.init()
+screen = pygame.display.set_mode((650, 500), pygame.RESIZABLE, 32)
+pygame.display.set_caption("paint")
+
+# <editor-fold desc="varibles">
+# COLOR
+color_bottle_background = (189, 189, 189)
+color_menu_tools = (255, 255, 255)
+
+# LOCATION (resizable)
+location_menu = (0, 0, 650, 60)
+location_tool = (0, 70, 70, 430)
+
+# SUBSURFACE
+sub_menu = screen.subsurface(location_menu)
+sub_tool = screen.subsurface(location_tool)
+# </editor-fold>
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit(0)
+        if event.type == VIDEORESIZE:
+            size = event.size
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE, 32)
+    screen.fill(color_bottle_background)
+    sub_menu.fill(color_menu_tools)
+    sub_tool.fill(color_menu_tools)
+    pygame.display.update()
---
+++ components/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2017/11/11 10:15
+# @Author  : leolee
+# @File    : __init__.py.py
